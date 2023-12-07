6caa2a16d1d109e4e194ba64200b80b2ef79e1da
Samay Navale <92618552+SamayXD@users.noreply.github.com>
2023-12-07 17:11:52 +0530

    Keeping Update

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -5,8 +5,42 @@
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 */
 
+// function calculateTotalSpentByCategory(transactions) {
+//   final_total = [];
+//   list_of_categories = [];
+//   temp_list = {category: "",price: 0}
+//   let rough = 0
+//   for(let i = 0; i<transactions.length; i++){
+//     if(!list_of_categories.includes(transactions[i].category)){
+//       list_of_categories.push(transactions[i].category)
+//     }else{
+//         // rough = temp_list.price + transactions[i].price
+//         temp_list = {category: transactions[i].category,price: transactions[i].price }
+//         final_total.push(temp_list)
+//     }
+//   }
+
+//   return final_total;
+// }
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const categoryTotals = {};
+
+  transactions.forEach(transaction => {
+      const { category, price } = transaction;
+      if (category in categoryTotals) {
+          categoryTotals[category] += price;
+      } else {
+          categoryTotals[category] = price;
+      }
+  });
+
+  return Object.entries(categoryTotals).map(([category, totalSpent]) => ({
+      category,
+      totalSpent,
+  }));
 }
 
+
+
+// console.log(calculateTotalSpentByCategory(sampleinput))
 module.exports = calculateTotalSpentByCategory;
--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -1,12 +1,18 @@
 /*
-  Write a function `findLargestElement` that takes an array of numbers and returns the largest element.
-  Example:
-  - Input: [3, 7, 2, 9, 1]
-  - Output: 9
+  Write a function `findLargestElement` that takes an array of numbers and returns the largest element.
+  Example:
+  - Input: [3, 7, 2, 9, 1]
+  - Output: 9
 */
 
 function findLargestElement(numbers) {
+    let newNums = numbers.sort((a, b) => b - a)
+    if(numbers.length == 0){
     
+    }else{
+        return newNums[0]
+    }
 }
 
+// console.log(findLargestElement([3, 7, 2, 9, 1]))
 module.exports = findLargestElement;
